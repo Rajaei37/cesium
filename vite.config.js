f7015a44dc72f64e3bfb33e6b73f54d72511638e
Rajaei Hammoud <ragaee37@gmail.com>
2025-07-14 16:42:39 -0400

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/build/', // Updated for root domain deployment
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
